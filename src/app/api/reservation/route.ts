dcb2ea6b2449cced9dc141eed770a6e917b1f84f
Katerina <benthebeaglecto@gmail.com>
2026-06-10 17:00:46 +0200

    fogfdsfsf

--- src/app/api/reservation/route.ts
+++ src/app/api/reservation/route.ts
@@ -28,19 +28,23 @@ Nachricht:
 ${body.message}
 `;
 
-  await fetch(
-    `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        chat_id: process.env.TELEGRAM_CHAT_ID,
-        text: message,
-      }),
-    }
-  );
+  const response = await fetch(
+  `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: process.env.TELEGRAM_CHAT_ID,
+      text: message,
+    }),
+  }
+);
 
-  return NextResponse.json({ success: true });
+const data = await response.json();
+
+console.log(data);
+
+return NextResponse.json(data);
 }
